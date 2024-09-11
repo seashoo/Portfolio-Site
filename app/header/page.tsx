0bc3f5ce56d098a4cb9232575604a83c6251382d
ashoorsahran <ashoorsahran@gmail.com>
2024-09-11 02:34:59 -0500

    test

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -9,15 +9,15 @@ export default function Header() {
   return (
     <section>
   <div className="flex flex-row items-center justify-center text-light-black">
-    <div className="bg-light-black w-2 mt-12"></div>
-    <div className="mt-8 ml-4 mr-4 w-fit text-center whitespace-nowrap">
+    <div className="bg-light-black w-2 h-12"></div>
+    <div className="mt-8 ml-4 mr-4 text-center whitespace-nowrap">
       <span className="text-5xl tracking-wider lg:text-9xl md:text-7xl lg:tracking-wide">Sahran Ashoor</span>
       <div className="bg-red h-8 md:mb-2"></div>
       <span className="text-xs lg:text-3xl md:text-lg tracking-wider">
         Computer Engineering @ The University of Houston
       </span>
     </div>
-    <div className="bg-light-black w-2 mt-12"></div>
+    <div className="bg-light-black w-2 h-12"></div>
   </div>
   <HeaderImages />
 </section>
