11e9395ba02093a3d5f00e18c014fd5771100840
ashoorsahran <ashoorsahran@gmail.com>
2024-09-11 02:37:27 -0500

    test

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -8,18 +8,18 @@ export default function Header() {
   
   return (
     <section>
-  <div className="flex flex-row items-center justify-center text-light-black">
-    <div className="bg-light-black w-4 h-24"></div> <!-- Adjusted width and height for larger bars -->
-    <div className="mt-8 mx-4 text-center whitespace-nowrap">
-      <span className="text-5xl tracking-wider lg:text-9xl md:text-7xl lg:tracking-wide">Sahran Ashoor</span>
-      <div className="bg-red h-8 md:mb-2"></div>
-      <span className="text-xs lg:text-3xl md:text-lg tracking-wider">
-        Computer Engineering @ The University of Houston
-      </span>
-    </div>
-    <div className="bg-light-black w-4 h-24"></div> 
-  </div>
-  <HeaderImages />
-</section>
+      <div className="flex flex-row justify-center text-light-black">
+        <div className="bg-light-black w-2 mt-12"></div>
+        <div className="mt-8 ml-4 mr-4 w-fit whitespace-nowrap">
+          <span className="text-5xl tracking-wider lg:text-9xl md:text-7xl lg:tracking-wide">Sahran Ashoor</span>
+          <div className="bg-red h-8 md:mb-2"></div>
+          <span className="text-xs lg:text-3xl md:text-lg justify-between tracking-wider">
+            Computer Engineering @ The University of Houston
+          </span>
+        </div>
+        <div className="bg-light-black w-2 mt-12"></div>
+      </div>
+      <HeaderImages />
+    </section>
   );
 }
