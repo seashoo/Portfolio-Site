31e8e2ad3e316d236dbf790bb25aa0d891bd2ace
ashoorsahran <ashoorsahran@gmail.com>
2024-09-11 02:43:30 -0500

    test

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -7,12 +7,12 @@ import HeaderImages from "../components/HeaderImages";
 export default function Header() {
   
   return (
-      <section>
+    <section>
     <div className="flex flex-row justify-center text-light-black">
       <div className="bg-light-black w-2 mt-12"></div>
       <div className="mt-8 ml-4 mr-4 w-fit whitespace-nowrap flex flex-col items-center">
         <span className="text-5xl tracking-wider lg:text-9xl md:text-7xl lg:tracking-wide">Sahran Ashoor</span>
-        <div className="bg-red h-8 md:mb-2"></div> 
+        <div className="bg-red-500 h-8 w-full md:mb-2"></div> <!-- Explicit height and color -->
         <span className="text-xs lg:text-3xl md:text-lg tracking-wider text-center">
           Computer Engineering @ The University of Houston
         </span>
