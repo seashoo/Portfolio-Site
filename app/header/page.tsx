7cf72d0933bd079861c5f2abb6cdef94116b25f0
ashoorsahran <ashoorsahran@gmail.com>
2024-09-11 02:53:42 -0500

    test

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         <div className="bg-light-black w-2 mt-12"></div>
         <div className="mt-8 ml-4 mr-4 w-fit whitespace-nowrap">
           <span className="text-5xl tracking-wider lg:text-9xl md:text-7xl lg:tracking-wide">Sahran Ashoor</span>
-          <div className="bg-red h-8 md:mb-2"></div>
+          <div className="bg-blue h-8 md:mb-2"></div>
           <span className="text-xs lg:text-3xl md:text-lg justify-between tracking-wider">
             Computer Engineering Student @ The University of Houston
           </span>
