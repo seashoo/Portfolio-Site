35c200688b26bf8bb3f7d57105bddcc3b6d9087f
ashoorsahran <ashoorsahran@gmail.com>
2024-09-26 23:02:35 -0500

    test

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,7 @@
 export const projectData = [
   {
-    image: "/bakermuseum.svg",
-    title: "Baker Museum",
+    
+    title: "SortSite",
     description:
       "This initiative was undertaken as part of the Database Systems course (COSC 3380) at the University of Houston. Our aim was to design and implement a sophisticated database system tailored for an art museum environment.",
     icons: ["reactjs", "javascript", "mysql", "node", "postman", "azure", "threejs" ,"tailwindcss", "git", "figma"],
@@ -43,59 +43,5 @@ export const projectData = [
     presentation: "https://youtu.be/37vrC4Xpajg?si=2DMGUyDueTuIlPty",
     link: "http://stellarscholars.tech/",
   },
-  {
-    image: "/bloom.svg",
-    title: "Bloom",
-    description:
-      "Your digital wellness companion. Cultivate mental well-being through gamified tasks and personalized tree nurturing. Elevating mental health with compassion and innovation.",
-    icons: ["figma"],
-    code: "",
-    demo: "",
-    presentation: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FtbX9jNSBJlXaXvGx6PWDHX%2Fbloom---Catalyst-Designathon%3Ftype%3Ddesign%26node-id%3D65-1407%26t%3DYxGivtXy8oCqaFMq-1%26scaling%3Dscale-down%26page-id%3D2%253A4%26starting-point-node-id%3D65%253A1407%26mode%3Ddesign",
-    link: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FtbX9jNSBJlXaXvGx6PWDHX%2Fbloom---Catalyst-Designathon%3Ftype%3Ddesign%26node-id%3D65-1407%26t%3DYxGivtXy8oCqaFMq-1%26scaling%3Dscale-down%26page-id%3D2%253A4%26starting-point-node-id%3D65%253A1407%26mode%3Ddesign",
-  },
-  {
-    image: "/cfg.svg",
-    title: "JPMC Code for Good: CanCare",
-    description:
-      "We enhanced UI/UX design for 100% form completion, optimized the matching algorithm for reduced latency, and introduced an AI chatbot for clients' instant access, securing 1st place in the competition.",
-    icons: ["python", "vite", "reactjs", "typescript", "tailwindcss", "git", "figma", "firebase", "flask", "numpy", "pytorch", "llama-index", "matplotlib", "scikit-learn"],
-    code: "",
-    demo: "",
-    presentation: "https://youtu.be/5AnWT0wbHak",
-    link: "https://www.youtube.com/embed/5AnWT0wbHak?si=jyczimiQ6PAIwhWC",
-  },
-  {
-    image: "/dataSoc.svg",
-    title: "Data & Society: Data Visualization Project",
-    description:
-      "Leveraging Kepler.gl for dynamic data visualization, we crafted linear regression models and a user-friendly website, merging insights with ease.",
-    icons: ["python", "keplergl", "nextjs", "typescript", "tailwindcss", "git", "pandas", "numpy", "scikit-learn", "matplotlib", "figma"],
-    code: "https://github.com/Ashishjob/HON-3397-Website",
-    demo: "https://hon-3397-website.vercel.app/",
-    presentation: "https://docs.google.com/presentation/d/1JfcGZgNcTMChKZFgFx2aYGrDBVClSnuJjORsl8ye5jo/edit?usp=sharing",
-    link: "https://hon-3397-website.vercel.app/",
-  },
-    {
-      image: "/portfolioProject.svg",
-      title: "portfolio",
-      description:
-        "A professionally crafted responsive website designed to showcase a diverse array of projects and professional experiences.",
-      icons: ["nextjs", "typescript", "tailwindcss", "git", "figma"],
-      code: "https://github.com/seashoo/Portfolio-Site",
-      demo: "https://seashoo.me",
-      presentation: "",
-      link: "http://localhost:3000/",
-    },
-    {
-      image: "/caseQuest.svg",
-      title: "MISSO x CougarCS Case Quest: Uni-Link",
-      description:
-        "We secured 3rd place by collaboratively developing an online marketplace for students. Utilizing Figma, we crafted a working prototype, system design, and userflow to achieve our goal",
-      icons: ["figma"],
-      code: "",
-      demo: "https://www.figma.com/proto/yxq73L0ROn9tvJZSwPrB6D/Untitled?node-id=2-3&starting-point-node-id=2%3A3&mode=design&t=IARvrb7DNLPQc4Th-1",
-      presentation: "https://docs.google.com/presentation/d/1UH5isF9kVRYcIzQqBLpSj8_o4gKgDN35M_T61vbZO_c/edit#slide=id.g35f391192_00",
-      link: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fyxq73L0ROn9tvJZSwPrB6D%2FUntitled%3Fnode-id%3D2-3%26starting-point-node-id%3D2%253A3%26t%3DgXdH5qMbaidiTPAi-1%26mode%3Ddesign",
-    },
+
   ];
