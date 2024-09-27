d89eea4f0f0a6379b3ee1763c7947bfc04809c13
ashoorsahran <ashoorsahran@gmail.com>
2024-09-27 01:13:41 -0500

    test

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,7 +4,7 @@ export const projectData = [
     title: "SortSite",
     description:
       "This initiative was undertaken as part of the Database Systems course (COSC 3380) at the University of Houston. Our aim was to design and implement a sophisticated database system tailored for an art museum environment.",
-    icons: ["reactjs", "javascript", "mysql", "node", "postman", "azure", "threejs" ,"tailwindcss", "git", "figma"],
+    icons: ["reactjs", "javascript", "mysql", "node", "postman", "azure", "threejs" ,"css3", "git", "figma"],
     code: "https://github.com/seashoo/SortSite",
     demo: "https://seashoo.github.io/SortSite/#/",
     presentation: "",
@@ -15,7 +15,7 @@ export const projectData = [
     title: "LiDAR-CV",
     description:
       "GitStudy is a study group platform that we built for hackMISSO 2024 with Next.js. It allows users to create and join study groups, share resources, and collaborate on projects.",
-    icons: ["nextjs", "typescript", "supabase", "shadcn", "tailwindcss", "git", "figma"],
+    icons: ["nextjs", "typescript", "supabase", "shadcn", "css3", "git", "figma"],
     code: "https://github.com/Brodypen/GitStudy",
     demo: "https://git-study-self.vercel.app/",
     presentation: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FcHS42y8ooT8eriJFxXPJQL%2FGitStudy---HackMISSO%3Ftype%3Ddesign%26node-id%3D20-179%26t%3DVLp4eX3kwuNkFgHm-1%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D20%253A179%26mode%3Ddesign",
@@ -26,7 +26,7 @@ export const projectData = [
     title: "Decipher.AI",
     description:
       "Create professional product mockups effortlessly with our customizable tool, perfect for e-commerce sites, offering a quick and user-friendly experience. If you need inspiration, explore our curated sets of mockups.",
-    icons: ["javascript", "reactjs", "tailwindcss", "git", "figma"],
+    icons: ["javascript", "reactjs", "css3", "git", "figma"],
     code: "",
     demo: "https://templit.net/",
     presentation: "",
@@ -37,7 +37,7 @@ export const projectData = [
     title: "Portfolio Website",
     description:
       "This is a short educational game utilizing cloudflare pages and OpenAI's GPT API to educate young minds on the important Concepts of the Galaxy and Gravity.",
-    icons: ["reactjs", "pixijs", "javascript", "openai", "tailwindcss", "git", "figma"],
+    icons: ["reactjs", "pixijs", "javascript", "openai", "css3", "git", "figma"],
     code: "https://github.com/brohudev/stellar-scholar",
     demo: "http://stellarscholars.tech/",
     presentation: "https://youtu.be/37vrC4Xpajg?si=2DMGUyDueTuIlPty",
