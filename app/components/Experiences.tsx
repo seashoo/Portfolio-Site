262dff1622e816d837d920558581288ba80557cf
ashoorsahran <ashoorsahran@gmail.com>
2024-09-29 09:54:25 -0500

    test

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -3,24 +3,24 @@ export const experiences = [
         position: "PROMES Workshop Facilitator - University of Houston",
         time: "[ August 2024 - Present ]",
         url: "https://promes.egr.uh.edu/",
-        description: "I will be working on the CRONUS FOD CI2 Team on Gateway and ISS projects related to developing databases, but more to come soon!"
+        description: "At the University of Houston, I lead a 1-credit hour workshop section for ENGI 1331 – Computing & Problem Solving for Engineers. In this role, I provide both lecture-based instruction and personalized one-on-one tutoring, with a focus on MATLAB. I design custom-tailored materials and study guides to meet the needs of over 50 students, ensuring alignment with course curricula and facilitating a deeper understanding of engineering principles."
     },
     {
         position: "Software Development Engineer Intern - Amazon Web Services",
         time: "[ May 2024 - August 2024 ]",
         url: "https://aws.amazon.com/",
-        description: "I led the frontend engineering for the application, and helped achieve a growth of 2000+ users and 100+ resume submissions within the first month through strategic marketing"
+        description: "As a Software Development Engineer intern at AWS, I was responsible for writing and publishing DynamoDB DAX Cluster controls for the Controls Tower platform, ensuring secure data encryption in transit across global regions using AWS Guard and CloudFormation. I developed a Python-based, Git-inspired command-line interface toolset to automate client-host interaction, with a tailored notification system that improved workflow efficiency across the company. Working independently as the sole SDE on a team of security research engineers, I collaborated closely with an adjacent software team to deploy proactive security controls in development environments."
     },
     {
         position: "Travel Intern & Technical Lead - Engineers Without Borders UH",
         time: "[ Sept 2023 - June 2024 ]",
         url: "https://www.uh.edu/nsm/computer-science/",
-        description: "I supported 400+ students in structured procedural programming and elementary functions in Python through code-reviewing, and provided feedback for multiple Python programming assignments."
+        description: "During my time with Engineers Without Borders UH, I traveled to Valle Valerio, Nicaragua as part of a 7-person team to implement a community water pump redesign. My role involved designing a robust electrical system interfacing with a 220VAC power supply, ensuring long-term reliability through compliance with safety standards and proper grounding. In addition to my technical contributions, I led a team of developers in updating and maintaining the EWB-UH website, creating an administrator login system with a Django back-end and MySQL database to support ongoing project management."
     },
     {
         position: "Security Systems Intern - Olezka Global",
         time: "[ Sept 2023 - May 2024 ]",
         url: "https://www.jpmorganchase.com/",
-        description: "I worked on developing cloud monitoring via upgrading private/on-prem cloud monitoring and integrating public Azure monitoring, refactoring the existing code for significant optimization, proposing AI integrations into the team's existing product, and working on an internal RAG-based copilot tool to optimize developer efficiency."
+        description: "At Olezka Global, I conducted network traffic analysis using Wireshark to identify and address potential security vulnerabilities. I also implemented network monitoring solutions with Auvik, which enhanced real-time diagnostics and overall system security. My work involved presenting detailed findings and recommendations to school leadership, contributing to the secure infrastructure setup for multiple campuses in Houston prior to their opening in early 2024."
     },    
 ]
