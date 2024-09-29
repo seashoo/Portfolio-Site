74ae05093499a116b57997ffe00719e3d925b03c
ashoorsahran <ashoorsahran@gmail.com>
2024-09-29 09:59:19 -0500

    test

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -3,24 +3,24 @@ export const experiences = [
         position: "PROMES Workshop Facilitator - University of Houston",
         time: "[ August 2024 - Present ]",
         url: "https://promes.egr.uh.edu/",
-        description: "At the University of Houston, I lead a 1-credit hour workshop section for ENGI 1331 – Computing & Problem Solving for Engineers. In this role, I provide both lecture-based instruction and personalized one-on-one tutoring, with a focus on MATLAB. I design custom-tailored materials and study guides to meet the needs of over 50 students, ensuring alignment with course curricula and facilitating a deeper understanding of engineering principles."
+        description: "In my role as a workshop facilitator for ENGI 1331 – Computing & Problem Solving for Engineers, I provide both lecture-based instruction and personalized tutoring focused on MATLAB. I design custom materials and study guides to meet the needs of over 50 students, enhancing their understanding of key engineering concepts."
     },
     {
         position: "Software Development Engineer Intern - Amazon Web Services",
         time: "[ May 2024 - August 2024 ]",
         url: "https://aws.amazon.com/",
-        description: "As a Software Development Engineer intern at AWS, I was responsible for writing and publishing DynamoDB DAX Cluster controls for the Controls Tower platform, ensuring secure data encryption in transit across global regions using AWS Guard and CloudFormation. I developed a Python-based, Git-inspired command-line interface toolset to automate client-host interaction, with a tailored notification system that improved workflow efficiency across the company. Working independently as the sole SDE on a team of security research engineers, I collaborated closely with an adjacent software team to deploy proactive security controls in development environments."
+        description: "As an intern at AWS, I developed and published DynamoDB DAX Cluster controls for the Controls Tower platform, ensuring secure data encryption using AWS Guard and CloudFormation. I also created a Python-based command-line interface toolset that automated client-host interactions, collaborating closely with security research engineers to deploy effective security controls."
     },
     {
         position: "Travel Intern & Technical Lead - Engineers Without Borders UH",
         time: "[ Sept 2023 - June 2024 ]",
         url: "https://www.uh.edu/nsm/computer-science/",
-        description: "During my time with Engineers Without Borders UH, I traveled to Valle Valerio, Nicaragua as part of a 7-person team to implement a community water pump redesign. My role involved designing a robust electrical system interfacing with a 220VAC power supply, ensuring long-term reliability through compliance with safety standards and proper grounding. In addition to my technical contributions, I led a team of developers in updating and maintaining the EWB-UH website, creating an administrator login system with a Django back-end and MySQL database to support ongoing project management."
+        description: "I traveled to Valle Valerio, Nicaragua, as part of a 7-person team to redesign and automate a community water pump system. My contribution ensured circuit safety and reliability while I was also leading a team to update the EWB-UH website with a new Django back-end and MySQL database."
     },
     {
         position: "Security Systems Intern - Olezka Global",
         time: "[ Sept 2023 - May 2024 ]",
         url: "https://www.jpmorganchase.com/",
-        description: "At Olezka Global, I conducted network traffic analysis using Wireshark to identify and address potential security vulnerabilities. I also implemented network monitoring solutions with Auvik, which enhanced real-time diagnostics and overall system security. My work involved presenting detailed findings and recommendations to school leadership, contributing to the secure infrastructure setup for multiple campuses in Houston prior to their opening in early 2024."
+        description: "During my internship at Olezka Global, I conducted network traffic analysis using Wireshark to identify security vulnerabilities and implemented monitoring solutions with Auvik. I presented my findings to school leadership, contributing to the secure setup of infrastructure for multiple campuses in Houston."
     },    
 ]
