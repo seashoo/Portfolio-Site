9d4af971e3523826a25e1d91f58fc332944672a8
ashoorsahran <ashoorsahran@gmail.com>
2024-09-29 10:02:23 -0500

    test

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -14,13 +14,13 @@ export const experiences = [
     {
         position: "Travel Intern & Technical Lead - Engineers Without Borders UH",
         time: "[ Sept 2023 - June 2024 ]",
-        url: "https://www.uh.edu/nsm/computer-science/",
+        url: "https://www.ewb-usa.org/",
         description: "I traveled to Valle Valerio, Nicaragua, as part of a 7-person team to redesign and automate a community water pump system. My contribution ensured circuit safety and reliability while I was also leading a team to update the EWB-UH website with a new Django back-end and MySQL database."
     },
     {
         position: "Security Systems Intern - Olezka Global",
         time: "[ Sept 2023 - May 2024 ]",
-        url: "https://www.jpmorganchase.com/",
+        url: "https://olezkaglobal.com/",
         description: "During my internship at Olezka Global, I conducted network traffic analysis using Wireshark to identify security vulnerabilities and implemented monitoring solutions with Auvik. I presented my findings to school leadership, contributing to the secure setup of infrastructure for multiple campuses in Houston."
     },    
 ]
