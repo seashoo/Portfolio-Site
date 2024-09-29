98fa10235598bef4bda59cf33c68ee1f445bbff1
ashoorsahran <ashoorsahran@gmail.com>
2024-09-29 04:22:29 -0500

    test

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,7 +11,7 @@ export const projectData = [
     link: "https://seashoo.github.io/SortSite/#/",
   },
   {
-    image: "/lidarcollage.svg",
+    image: "lidarcollage.svg",
     title: "LiDAR-CV",
     description:
       "This project uses a Benewake TFMini Plus LiDAR module mounted to scan 225 degrees on a vertical axis. A hall effect angle encoder reports the angular position of the scan rotor.",
